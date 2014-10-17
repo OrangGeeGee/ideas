71e4e706d90b04f7dd8e307a4d9ac3457676f3bf
msald <msald@a47fb915-6441-4bf9-b1ea-8e8557306987>
2014-10-17 13:10:46 +0000

    Fixed LDAP authentication.
    git-svn-id: https://svn.crebit.ee/internal/brainstorm@31 a47fb915-6441-4bf9-b1ea-8e8557306987

--- app/ldap-auth.php
+++ app/ldap-auth.php
@@ -1,65 +1,90 @@
 <?php
 
-define('GLOBAL_USER_KEY', 'PHP_AUTH_USER');  # Either PHP_AUTH_USER or REMOTE_USER.
-define('LDAP_DOMAIN', '@INT.HANSA.EE');
-define('LDAP_DEFAULT_USER_ID', 'msald');  # Set this to your own for local dev.
-define('LDAP_ERR_CONNECT', 'Could not connect to LDAP server.');
-define('LDAP_ERR_BIND', 'Error trying to bind: %s');
-define('LDAP_ERR_SEARCH', 'Error in search query: %s');
-define('LDAP_ERR_USER_NOT_FOUND', 'User "%s" not found.');
+/**
+ * Integrating this script into your project will give you access to the
+ * basic data of the user that's accessing the site. Just include the script
+ * and call getActiveUserDataViaLDAP() function. Needs php_ldap module to be
+ * enabled.
+ *
+ * @author  Mattias Saldre
+ * @updated 2014-02-03
+ */
+
+define('LDAP_GLOBAL_USER_KEY', 'PHP_AUTH_USER');  # Either PHP_AUTH_USER or REMOTE_USER.
+define('LDAP_DOMAIN_OLD',      '@INT.HANSA.EE');
+define('LDAP_DOMAIN_NEW',      '@FSPA.MYNTET.SE');
+define('LDAP_SERVER',          'ldap-test.int.hansa.ee');
+define('LDAP_TREE',            'OU=Employees,OU=Accounts,DC=int,DC=hansa,DC=ee');
+define('LDAP_BIND_TREE',       'CN=diego,OU=Service Accounts,OU=Accounts,DC=int,DC=hansa,DC=ee');
+define('LDAP_BIND_PASSWORD',   '34VeaZyv4mKHkfCoGscWIw2r');
+define('LDAP_FILTER',          '(&(objectClass=user)(samaccountName=%s))');
+define('LDAP_DEFAULT_USER_ID', 't018ttt');
+
+define('LDAP_ERROR_CONNECT',   'Could not connect to LDAP server.');
+define('LDAP_ERROR_BIND',      'Error trying to bind: %s');
+define('LDAP_ERROR_SEARCH',    'Error in search query: %s');
+define('LDAP_ERROR_USER_NOT_FOUND', 'User "%s" not found.');
 
 
 /**
  * @param string $message
  * @param string $details
- * @param int    $code
+ * @throws Exception
  */
-function abort($message = '', $details = '', $code = 404) {
-  App::abort($code, sprintf($message, $details));
-  exit;
+function throwException($message = '', $details = '') {
+  throw new Exception(sprintf($message, $details));
 }
 
 
 /**
- * Verify the user.
- * ----------------------------------------------------------------------------
+ * Retrieves information about the current user. Falls back to
+ * LDAP_DEFAULT_USER_ID if the user name can't be resolved.
+ *
+ * @return object
  */
-$userAccountId = isset($_SERVER[GLOBAL_USER_KEY])
-  ? str_replace(LDAP_DOMAIN, '', $_SERVER[GLOBAL_USER_KEY])
-  : LDAP_DEFAULT_USER_ID;
+function getActiveUserDataViaLDAP() {
+  $userAccountId = isset($_SERVER[LDAP_GLOBAL_USER_KEY])
+                 ? str_replace(array(LDAP_DOMAIN_OLD, LDAP_DOMAIN_NEW), '', $_SERVER[LDAP_GLOBAL_USER_KEY])
+                 : LDAP_DEFAULT_USER_ID;
+  $config = (object) array(
+    'server'     => LDAP_SERVER,
+    'tree'       => LDAP_TREE,
+    'filter'     => sprintf(LDAP_FILTER, $userAccountId),
+    'attributes' => array('displayname', 'mail')
+  );
+  $conn    = ldap_connect($config->server)
+               or throwException(LDAP_ERROR_CONNECT);
+  $binding = ldap_bind($conn, LDAP_BIND_TREE, LDAP_BIND_PASSWORD)
+               or throwException(LDAP_ERROR_BIND, ldap_error($conn));
+  $result  = ldap_search($conn, $config->tree, $config->filter, $config->attributes)
+              or throwException(LDAP_ERROR_SEARCH, ldap_error($conn));
+  $data    = ldap_get_entries($conn, $result);
 
-$config = (object) array(
-  'server'     => 'ldap-test.int.hansa.ee',
-  'tree'       => 'OU=Employees,OU=Accounts,DC=int,DC=hansa,DC=ee',
-  'filter'     => "(&(objectClass=user)(samaccountName={$userAccountId}))",
-  'attributes' => array('displayname', 'mail')
-);
+  # Results have been fetched, no more point in keeping the connection open.
+  ldap_close($conn);
 
-$conn    = ldap_connect($config->server)
-             or abort(LDAP_ERR_CONNECT);
-$binding = ldap_bind($conn)
-             or abort(LDAP_ERR_BIND, ldap_error($conn));
-$result  = ldap_search($conn, $config->tree, $config->filter, $config->attributes)
-             or abort(LDAP_ERR_SEARCH, ldap_error($conn));
-$data    = ldap_get_entries($conn, $result);
+  if ( $data['count'] == 0 )
+  {
+    throwException(LDAP_ERROR_USER_NOT_FOUND, $userAccountId);
+  }
 
-# Results have been fetched, no more point in keeping the connection open.
-ldap_close($conn);
-
-if ( $data['count'] == 0 )
-{
-  abort(LDAP_ERR_USER_NOT_FOUND, $userAccountId, 301);
+  return (object) array(
+    'id' => $userAccountId,
+    'name' => $data[0]['displayname'][0],
+    'email' => $data[0]['mail'][0]
+  );
 }
 
-$user = User::find($userAccountId);
+$userData = getActiveUserDataViaLDAP();
+$user = User::find($userData->id);
 
 # The user isn't registered in our database.
 if ( !$user )
 {
   $user = User::create(array(
-    'id' => $userAccountId,
-    'name' => $data[0]['displayname'][0],
-    'email' => $data[0]['mail'][0]
+    'id' => $userData->id,
+    'name' => $userData->name,
+    'email' => $userData->email
   ));
 }
 
