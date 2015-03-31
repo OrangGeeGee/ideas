076e4d909bc833375602dd427e52019569f7ce9a
msald <msald@a47fb915-6441-4bf9-b1ea-8e8557306987>
2015-03-31 15:25:38 +0000

    Provide a link to the idea that got voted for.
    git-svn-id: https://svn.crebit.ee/internal/brainstorm@80 a47fb915-6441-4bf9-b1ea-8e8557306987

--- app/views/emails/vote.blade.php
+++ app/views/emails/vote.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
-		<h2>Brainstorm</h2>
-	  <p>{{ $user->name }} just voted for your idea "{{ $idea->title }}".</p>
+    <h2>Angaar</h2>
+    <p>{{ $user->name }} andis oma hääle sinu ideele <a href="http://eos.crebit.ee/brainstorm/#ideas/{{ $idea->id }}" target="_blank">"{{ $idea->title }}"</a>.</p>
 	</body>
 </html>
