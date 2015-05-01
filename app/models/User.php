f26ad0304b45d0300973a320bc5d634cd122837c
msald <msald@a47fb915-6441-4bf9-b1ea-8e8557306987>
2015-05-01 09:33:37 +0000

    A small hint for secretaries on what to do when a new idea is submitted.
    git-svn-id: https://svn.crebit.ee/internal/brainstorm@103 a47fb915-6441-4bf9-b1ea-8e8557306987

--- app/models/User.php
+++ app/models/User.php
@@ -42,6 +42,11 @@ class User extends Eloquent implements UserInterface {
     return true;
   }
 
+  public function getFirstName()
+  {
+    return explode(' ', $this->name)[0];
+  }
+
   public function hasEstonianEmailAddress()
   {
     return substr($this->email, -3) == '.ee';
--- app/views/emails/idea.blade.php
+++ app/views/emails/idea.blade.php
@@ -5,6 +5,6 @@
   </head>
   <body>
     <h2>Angaar</h2>
-    <p>{{ $user->name }} lisas Ideekeskkonda uue idee <a href="http://eos.crebit.ee/angaar/#ideas/{{ $idea->id }}" target="_blank">"{{ $idea->title }}"</a>.</p>
+    <p>{{ $user->name }} lisas Ideekeskkonda uue idee <a href="http://eos.crebit.ee/angaar/#ideas/{{ $idea->id }}" target="_blank">"{{ $idea->title }}"</a>. Kui {{ $user->getFirstName() }} töötab sinu korrusel, siis ole hea ja vii talle miskit magusat. :)</p>
   </body>
 </html>
