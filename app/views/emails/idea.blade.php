6270f01ae2bb2a7acea28d8c1d89d6acad120aca
msald <msald@a47fb915-6441-4bf9-b1ea-8e8557306987>
2015-05-05 10:17:49 +0000

    A small hint for secretaries on what to do when a new idea is submitted.
    git-svn-id: https://svn.crebit.ee/internal/brainstorm@104 a47fb915-6441-4bf9-b1ea-8e8557306987

--- app/views/emails/idea.blade.php
+++ app/views/emails/idea.blade.php
@@ -5,6 +5,6 @@
   </head>
   <body>
     <h2>Angaar</h2>
-    <p>{{ $user->name }} lisas Ideekeskkonda uue idee <a href="http://eos.crebit.ee/angaar/#ideas/{{ $idea->id }}" target="_blank">"{{ $idea->title }}"</a>. Kui {{ $user->getFirstName() }} töötab sinu korrusel, siis ole hea ja vii talle miskit magusat. :)</p>
+    <p>{{ $user->name }} lisas Ideekeskkonda uue idee <a href="http://eos.crebit.ee/angaar/#ideas/{{ $idea->id }}" target="_blank">"{{ $idea->title }}"</a>. Kui {{ $user->getFirstName() }} töötab sinu korrusel, siis ole hea ja vii talle miskit magusat. :) Kui sul on magus otsas (või Mattias ei ole sulle magusat toonudki), siis anna sellest <a href="mailto:mattias.saldre@swedbank.ee">Mattiasele</a> märku.</p>
   </body>
 </html>
