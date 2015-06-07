4adca0d1ce872b54d2512ed9278e0126378ffe7f
msald <msald@a47fb915-6441-4bf9-b1ea-8e8557306987>
2015-06-07 14:38:27 +0000

    Fixed migration name.
    git-svn-id: https://svn.crebit.ee/internal/brainstorm@108 a47fb915-6441-4bf9-b1ea-8e8557306987

--- app/database/migrations/2015_03_10_140521_rename_timestamps_to_voted_at_and_seen_at.php
+++ app/database/migrations/2015_03_10_140521_rename_timestamps_to_voted_at_and_seen_at.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateIdeaUserTable extends Migration {
+class RenameTimestampsToVotedAtAndSeenAt extends Migration {
 
 	/**
 	 * Run the migrations.
