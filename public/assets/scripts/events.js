0f6c40b05bc7f143bf4618095380596f1c83470c
msald <msald@a47fb915-6441-4bf9-b1ea-8e8557306987>
2015-07-05 15:27:34 +0000

    Removed Laravel 4.0.
    git-svn-id: https://svn.crebit.ee/internal/brainstorm@119 a47fb915-6441-4bf9-b1ea-8e8557306987

--- public/assets/scripts/events.js
+++
@@ -1,9 +0,0 @@
-
-var Events = new Collection(generateShadowEnvironmentLink('events'), function() {
-  function addEvent(event) {
-    Ideas.get(event.get('idea_id')).events.add(event);
-  }
-
-  this.each(addEvent);
-  this.on('add', addEvent);
-});
