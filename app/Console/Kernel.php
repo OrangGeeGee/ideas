902f59d838d5ff01c9b37a923a3c6bb04be86e8c
msald <msald@a47fb915-6441-4bf9-b1ea-8e8557306987>
2015-08-10 07:16:44 +0000

    Nope, command() method actually...
    git-svn-id: https://svn.crebit.ee/internal/brainstorm@157 a47fb915-6441-4bf9-b1ea-8e8557306987

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel {
 	protected function schedule(Schedule $schedule)
 	{
 		$schedule
-			->exec('brainstorm:dailyupdate')
+			->command('brainstorm:dailyupdate')
 			->dailyAt('08:00');
 	}
 
